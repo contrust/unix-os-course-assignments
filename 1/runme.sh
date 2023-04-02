8f332a5b8d7435e3cd8e12e001d597c0b1fd365b
Artyom Borisov <2001raf@gmail.com>
2023-04-02 12:10:43 +0500

    Modified runme.sh and result.txt

--- 1/runme.sh
+++ 1/runme.sh
@@ -8,5 +8,5 @@ gzip -k fileB
 gzip -cd fileB.gz | ./myprogram fileC
 ./myprogram -b 100 fileA fileD
 for filename in fileA fileA.gz fileB fileB.gz fileC fileD; do
-	echo "${filename} actual size: $(stat -c "%b * 512" $filename | bc)"
+	echo "${filename} actual size: $(stat -c "%b * 512" $filename | bc) bytes"
 done
