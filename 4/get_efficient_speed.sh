7590f0d08aa411d30dfcf4921305c1cd532275df
Artyom Borisov <2001raf@gmail.com>
2023-05-31 11:37:22 +0500

    Added the tmp paths in get_efficinet_speed.sh.

--- 4/get_efficient_speed.sh
+++ 4/get_efficient_speed.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "`grep "Seconds passed between the first and the last read:" server123.log | tail -n 1 | cut -d ' ' -f10` - `grep "Total delay is" client123.log | sort -nrk4,4 | head -1 | cut -d ' ' -f4`" | bc
+echo "`grep "Seconds passed between the first and the last read:" /tmp/server123.log | tail -n 1 | cut -d ' ' -f10` - `grep "Total delay is" /tmp/client123.log | sort -nrk4,4 | head -1 | cut -d ' ' -f4`" | bc
